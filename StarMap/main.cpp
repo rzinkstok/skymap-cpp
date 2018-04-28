835759ad73a514740d06d7528c2d16a2fbfe82a7
Roel Zinkstok <rzinkstok@gmail.com>
2018-04-28 18:55:35 +0200

    Initial Commit

---
+++ StarMap/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  StarMap
+//
+//  Created by Roel Zinkstok on 28/04/2018.
+//  Copyright © 2018 Roel Zinkstok. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
